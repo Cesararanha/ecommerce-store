853fd205dfc1c06cf7fd1d8214f23fc83a153111
César Aranha <cesararanha2009@gmail.com>
2025-08-14 02:04:03 -0300

    Implementação da funcionalidade do carrinho e sua esilização
    - Funcionalidade de adicionar e remove itens ao carrinho
    - Incremento e Decremento da quantidade de itens no carrinho
    - Vizualização da quantidade de itens no carrinho no headder
    - Toda estilização da página do carrinho e de suas funcionalidades
    - Ajuste de pequenos erros

---
+++ js/main.js
@@ -0,0 +1,115 @@
+// Cart
+let cartItems = JSON.parse(localStorage.getItem("cartItems") || "[]");
+
+// Add to cart
+function addToCart(productCard) {
+    const name = productCard.querySelector(".product-title").textContent;
+    const priceText = productCard.querySelector(".product-price").textContent;
+    const price = parseFloat(priceText.replace("$", ""));
+    const imgSrc = productCard.querySelector(".product-img").src;
+
+    const existingItem = cartItems.find((item) => item.name === name);
+    if (existingItem) {
+        existingItem.quantity += 1;
+    } else {
+        cartItems.push({
+            name,
+            price,
+            quantity: 1,
+            image: imgSrc,
+        })
+    }
+    updateLocalStorage();
+    updateCartCount();
+    if (document.getElementById("cartItems")) {
+        displayCartItems();
+    }
+}
+
+// Remove from cart
+function removeItem(name) {
+    cartItems = cartItems.filter((item) => item.name !== name);
+    updateLocalStorage();
+    updateCartCount();
+    if (document.getElementById("cartItems")) {
+        displayCartItems();
+    }
+}
+
+// Update cart count icon
+function updateCartCount() {
+    const countElement = document.getElementById("cart-count");
+    const itemCount = cartItems.reduce((count, item) => count + item.quantity, 0);
+    if (countElement) {
+        countElement.textContent = itemCount;
+    }
+}
+
+function displayCartItems() {
+    const cartContainer = document.getElementById("cartItems")
+    const totalElement = document.getElementById("cartTotal");
+
+    if (!cartContainer) { return }
+
+    cartContainer.innerHTML = "";
+    let total = 0;
+
+    cartItems.forEach((item) => {
+        const itemTotal = item.price * item.quantity;
+        total += itemTotal;
+        const cartItem = document.createElement("div");
+        cartItem.className = "cart-item";
+        cartItem.innerHTML = `<img src="${item.image}" alt="${item.name}">
+            <div class="cart-title-price">
+                <div class="cart-item-title">${item.name}</div>
+                <div class="cart-item-price">$${itemTotal.toFixed(2)}</div>
+            </div>
+            <div class="quantity-controls">
+                <button onclick="changeQuantity('${item.name}', -1)">
+                    <i class="ri-subtract-line"></i>
+                </button>
+                <input type="text" name="" class="cart-item-quantity" value="${item.quantity}" min="1"
+                    onchange="updateQuantity('${item.name}', this.value)" readonly>
+                    <button onclick="changeQuantity('${item.name}', +1)">
+                        <i class="ri-add-line"></i>
+                    </button>
+            </div>
+            <div class="remove-from-cart" onclick="removeItem('${item.name}')">
+                <i class="ri-delete-bin-line"></i>
+            </div>`;
+        cartContainer.appendChild(cartItem);
+
+    })
+    if (totalElement) {
+        totalElement.textContent = `Total: $${total.toFixed(2)}`;
+    }
+}
+
+// Quantity Change
+function changeQuantity(name, delta) {
+    const item = cartItems.find((item) => item.name === name);
+    if (item) {
+        item.quantity += delta;
+        if (item.quantity <= 0) {
+            removeItem(name);
+        } else {
+            updateLocalStorage();
+            updateCartCount();
+            displayCartItems();
+        }
+    }
+}
+
+
+// Save cart in Localstorage
+function updateLocalStorage() {
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+}
+
+// Load cart on page Load
+window.onload = function () {
+    updateCartCount();
+    if (document.getElementById("cartItems")) {
+        displayCartItems()
+    }
+}
